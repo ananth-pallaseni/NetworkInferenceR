b090cb057d40bbc2ccbe39c4cfbc2ea2def47c57
Tchanders <thalia.e.chan@googlemail.com>
2018-03-14 12:12:28 +0000

    Rename gene_data to something more generic

--- R/network_inference.R
+++ R/network_inference.R
@@ -1,14 +1,14 @@
 
 infer_network <- function(filepath, method="PID") {
-  gene_data <- t(read.table(filepath))
+  data <- t(read.table(filepath))
   if (method == "PID") {
-    network <- infer_pid_network(gene_data)
+    network <- infer_pid_network(data)
   }
   else if (method == "PUC") {
-    network <- infer_puc_network(gene_data)
+    network <- infer_puc_network(data)
   }
   else if (method == "MI") {
-    network <- infer_mi_network(gene_data)
+    network <- infer_mi_network(data)
   }
   else {
     errstr <- paste("Unrecognised method argument:", method, "Should be one of: PID, PUC or MI")
